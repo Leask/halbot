73f732be35f896281b13e5aec66cafd817b1781a
Leask Wong <i@leaskh.com>
2025-02-02 15:24:26 -0500

    fallback to trandition ocr and stt is model does not support attachments

--- skills/60_prepare.mjs
+++ skills/60_prepare.mjs
@@ -1,5 +1,21 @@
 import { alan, bot, utilitas } from 'utilitas';
 
+const checkUnsupportedMimeType = async ctx => {
+    ctx.carry.attachments = [];
+    for (const x of ctx.collected.filter(x => x.type === 'PROMPT')) {
+        let notSupported = false;
+        ctx.selectedAi.map(y => {
+            if (![
+                ...alan.MODELS[ctx._.ai[y].model]?.supportedMimeTypes || [],
+                ...alan.MODELS[ctx._.ai[y].model]?.supportedAudioTypes || [],
+            ].includes(y?.content?.mime_type)) { notSupported = true; }
+        });
+        notSupported ? await x.content.analyze() : ctx.carry.attachments.push({
+            ...x.content, analyze: undefined,
+        });
+    }
+};
+
 const action = async (ctx, next) => {
     // avatar
     if (ctx.result) {
@@ -16,9 +32,7 @@ const action = async (ctx, next) => {
         ctx.avatar = '😸';
     }
     // prompt
-    ctx.carry.attachments = ctx.collected.filter(
-        x => x.type === 'PROMPT'
-    ).map(x => x.content);
+    await checkUnsupportedMimeType(ctx);
     const maxInputTokens = alan.getMaxChatPromptLimit()
         - alan.ATTACHMENT_TOKEN_COST * ctx.carry.attachments.length;
     const additionInfo = ctx.collected.filter(
@@ -28,7 +42,7 @@ const action = async (ctx, next) => {
     while (await alan.countTokens(
         `${ctx.prompt}${additionInfo?.[0] || ''}`
     ) < maxInputTokens && additionInfo.length) {
-        ctx.prompt += ` ${additionInfo.shift()}`;
+        ctx.prompt += `${additionInfo.shift()} `;
     }
     ctx.prompt = utilitas.trim(ctx.prompt);
     additionInfo.filter(x => x).length && (ctx.prompt += '...');
