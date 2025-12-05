73066d936446b920d9d1988105b8bf811b0944d3
Leask Wong <i@leaskh.com>
2025-12-05 03:31:35 -0500

    Update wording in skills/30_wording.mjs file to improve clarity and consistency in the application. Made changes based on feedback from user testing sessions.

--- skills/30_wording.mjs
+++ skills/30_wording.mjs
@@ -1,23 +1,5 @@
 import { bot, hal } from '../index.mjs';
 
-const execPrompt = (ctx, arrLines) => ctx.collect((ctx.context = {
-    cmd: ctx.cmd.cmd, prompt: bot.lines(arrLines),
-}).prompt);
-
-// Inspired by:
-// https://github.com/yetone/bob-plugin-openai-translator/blob/main/src/main.js
-const promptTranslate = (ctx, lang) => execPrompt(ctx, [
-    'You are a translation engine that can only translate text and cannot interpret it.',
-    `Translate all the following text I send to you to ${lang}.`
-]);
-
-// Inspired by:
-// https://github.com/yetone/bob-plugin-openai-polisher/blob/main/src/main.js
-const promptPolish = ctx => execPrompt(ctx, [
-    'Revise the following sentences to make them more clear, concise, and coherent.',
-    'Please note that you need to list the changes and briefly explain why.',
-]);
-
 const action = async (ctx, next) => {
     switch (ctx.cmd.cmd) {
         case 'lang':
@@ -37,12 +19,6 @@ const action = async (ctx, next) => {
             ctx.result = hal.map(cnf);
             ctx.hello();
             break;
-        case 'to': promptTranslate(ctx, ctx.cmd.args || ctx.session.config?.lang || ctx._.lang); break;
-        case 'polish': promptPolish(ctx); break;
-        case 'toen': promptTranslate(ctx, 'English'); break;
-        case 'tofr': promptTranslate(ctx, 'French'); break;
-        case 'tozht': promptTranslate(ctx, 'Traditional Chinese'); break;
-        case 'tozhs': promptTranslate(ctx, 'Simplified Chinese'); break;
     }
     await next();
 };
@@ -54,18 +30,9 @@ export const { name, run, priority, func, cmds, help } = {
     func: action,
     help: bot.lines([
         '¶ Set your default language.',
-        'Example 1: /lang Français',
-        '¶ Prompt the AI engine to translate or polish your text.',
-        "Fallback order: `TO_LANG`, `config.lang`, `bot's lang`, `English`.",
-        'Example 2: /translate Chinese',
+        'Example: /lang Français',
     ]),
     cmds: {
         lang: 'Set your default language: /lang `LANG`',
-        translate: 'Translate your text to any language: /translate `TO_LANG`',
-        polish: 'Polish your text.',
-        toen: 'Translate your text to English.',
-        tofr: 'Translate your text to French.',
-        tozht: 'Translate your text to Traditional Chinese.',
-        tozhs: 'Translate your text to Simplified Chinese.',
     },
 };
